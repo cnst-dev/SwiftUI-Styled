e20cecf93be9570afcad0a087efa1f40884af52a
const <sqb@mail.ru>
2023-06-14 09:24:08 +0200

    Initial Commit

---
+++ SwiftUI-Styled/SwiftUI_StyledApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_StyledApp.swift
+//  SwiftUI-Styled
+//
+//  Created by const on 14.06.2023.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_StyledApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
